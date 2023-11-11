12917637efd247c09b52e5c5e2fe61b1f6fe1faf
Jaber Ibrahim <jaberebra@gmail.com>
2023-11-11 16:00:48 +0300

    edit some files

--- src/Sections/LastNews/LastNews.jsx
+++ src/Sections/LastNews/LastNews.jsx
@@ -1,13 +1,37 @@
 import {MainTitle , LastNewsCard } from "./../../Components/importComponents"
 import LastNewsData from './../../data/lastNewData';
+import axios from "./../../Components/axios/customAxios"
+import { useState , useEffect } from "react";
 
 const LastNews = () => {
-  let LastNewsCards = LastNewsData.map((card) => {
-    return (
-      <LastNewsCard key = {card.id}
-      title ={card.title}/>
-    )
-  })
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    axios.get("e22743e0-c909-497f-bd4d-9db1ca6246cd",news)
+      .then((response)=> {
+        setNews(response.data.data)
+        console.log(response.data.data)
+        if(news) {
+          console.log(news)
+          let LastNewsCards = LastNewsData.map((card) => {
+            return (
+              <LastNewsCard key = {card.id}
+              title ={card.title}/>
+            )
+          })
+        }
+      })
+      .catch((error) =>{
+        console.log(error)
+      })
+  }, []);
+  
+  // let LastNewsCards = LastNewsData.map((card) => {
+  //   return (
+  //     <LastNewsCard key = {card.id}
+  //     title ={card.title}/>
+  //   )
+  // })
   return (
     <>
         <MainTitle title="آخر الأخبار"/>
