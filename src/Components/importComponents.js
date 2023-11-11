1c6c8f119f6b243f75aec8964df1c1f1d25eefac
YaraDawa <yara.manhal.dawa@gmail.com>
2023-11-11 08:22:17 -0500

    create navbar ,services and footer

--- src/Components/importComponents.js
+++
@@ -1,6 +0,0 @@
-export {default as Header} from './Header/Header';
-export {default as Button} from './Button/Button';
-export {default as MainTitle} from './MainTitle/MainTitle';
-export {default as Container} from './Container/Container';
-export {default as Card} from './ServiceCard/Card';
-export {default as Footer} from './Footer/Footer';
--- src/Sections/importSections.js
+++
@@ -1,3 +0,0 @@
-export {default as Hero} from './Hero/Hero';
-export {default as MediaNews} from './MediaNews/MediaNews';
-export {default as Services} from './TheServices/Services';
